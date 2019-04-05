ac453474b4d542762282a2c255dda4ad96d6c5e8
arezki1 <a.ibsaine@hotmail.fr>
2019-04-06 00:36:23 +0100

    jj

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-
+  # test "the truth" do
+  #   assert true
+  # end
   def test_should_not_save_job_without_title
     job = Job.new
     assert !job.save
